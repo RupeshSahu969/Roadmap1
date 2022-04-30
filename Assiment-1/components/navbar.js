d2ce04165bac8ddf00cd66cdc1ebc5458c3acb31
Rupesh Kumar <rupeshsahu969@gmail.com>
2022-04-30 12:15:45 +0530

    +AAA+

---
+++ Assiment-1/components/navbar.js
@@ -0,0 +1,24 @@
+function navbar(){
+
+    return  `
+    <h3>
+    <a href="#">Home</a>
+    
+</h3>
+<h3>
+    <a href="#">About Us</a>
+    
+</h3>
+<h3>
+    <a href="#">Login</a>
+    
+</h3>
+<h3>
+    <a href="#">Signup</a>
+    
+</h3>
+</div>
+    `;
+}
+
+export { navbar }
---
+++ components/navbar.js
@@ -0,0 +1,24 @@
+function navbar(){
+
+    return  `
+    <h3>
+    <a href="/jewelery.html">jewelery</a>
+    
+</h3>
+<h3>
+    <a href="/electronic.html">Electronik</a>
+    
+</h3>
+<h3>
+    <a href="login.html">login</a>
+    
+</h3>
+<h3>
+    <a href="signup.html">signup</a>
+    
+</h3>
+</div>
+    `;
+}
+
+export { navbar }
